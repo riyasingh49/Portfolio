87980fbb3ecf698c71dfe33c609eab080526d7a9
riyasingh49 <riyasingh454247@gmail.com>
2025-06-05 12:14:01 +0530

    start for portfolio

---
+++ src/components/Navbars/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.jpg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src = {logo} alt=''/>
+      <ul className='nav-menu'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Courses & Certifications</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact info</li>
+      </ul>
+      <div className='nav-connect'>Connect with me</div>
+      
+    </div>
+  )
+}
+
+export default navbar
