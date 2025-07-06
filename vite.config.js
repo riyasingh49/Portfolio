fdeccc2ee6a4a3c5af11cf8daa045cce872976ca
riyasingh49 <riyasingh454247@gmail.com>
2025-07-06 16:32:21 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio/",  
   plugins: [react()],
   server: {
-    host: true, 
+    host: true,
     port: 5173,
   },
 })
