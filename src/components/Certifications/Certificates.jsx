a88155468ea71285d64fc2896bfafa19df5150de
riyasingh49 <riyasingh454247@gmail.com>
2025-08-03 00:00:00 +0530

    new certificate added

--- src/components/Certifications/Certificates.jsx
+++ src/components/Certifications/Certificates.jsx
@@ -4,6 +4,7 @@ import NPTEL from '../../assets/NPTEL.jpeg';
 import HTML from '../../assets/HTML.jpeg';
 import JavaScript from "../../assets/JavaScript.jpg";
 import algouniversity from "../../assets/algouniversity.png";
+import DataAnalysis from "../../assets/DataAnalysis.jpg"
 
 const Certificates = () => {
   return (
@@ -17,6 +18,8 @@ const Certificates = () => {
         <a href="https://drive.google.com/file/d/11HljzdDCclvHOyikWb6e-9wmwv1NAjxi/view?usp=sharing" target="_blank" rel = "noopener noreferrer" ><img src={HTML} alt="" /></a>
         <a href="https://drive.google.com/file/d/1-zGvWYe9JZWoSW8sYHuGYwa7m64cYlUg/view?usp=sharing " target="_blank" rel = "noopener noreferrer" ><img src={JavaScript} alt="" /></a>
         <a href="https://drive.google.com/file/d/15i1_OOhRk5q1_ORJHPTQyJoQKlc_1zlc/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src={algouniversity} alt="" /></a>
+        <a href="https://drive.google.com/file/d/1YJXqeDo-VloI_oI9Fbj7mWoy8-Z8Pq9Q/view?usp=sharing" target="_blank" rel = "noopener noreferrer" ><img src={DataAnalysis} alt="" /></a>
+
       </div>
       
     </div>
