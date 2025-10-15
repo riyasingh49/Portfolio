26d4dea3b7365fa25158ffd3486d7aae55449b83
riyasingh49 <riyasingh454247@gmail.com>
2025-10-15 14:05:40 +0530

    experience completed

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -5,17 +5,17 @@ const Experience = () => {
   return (
     <div className='Experience'>
         <div className="Exp-title">
-            <h1>My Work Experiences</h1>
+            <h1>My Work Experience</h1>
         </div>
 
         <div className="experience-box">
         
 
         <div className="Exp-Intro">
-            <h2>AICTE Virtual Internship - AI and Data Analytics Intern (Green Skills) </h2>
+            <h2> Virtual Internship - AI and Data Analytics Intern (Green Skills) </h2>
             <h3>AICTE x Shell x Edunet Foundation</h3>
             <h4>Aug 2025 - Sep 2025</h4>
-            <p>During my AICTE–Shell–Edunet Foundation internship on AI and Data Analytics, I built an Air Quality Forecast model using machine learning to predict AQI from environmental data. I handled data preprocessing, feature engineering, and model training using Python, Pandas, NumPy, Scikit-learn, and Matplotlib, enabling accurate air quality prediction and visualization.</p>
+            <p>During my AICTE–Shell–Edunet Foundation internship on AI and Data Analytics, I developed an Air Quality Forecast model using Python, Pandas, NumPy, and Scikit-learn for AQI prediction through data preprocessing, feature engineering, and model training.</p>
         </div>
       
         </div>
